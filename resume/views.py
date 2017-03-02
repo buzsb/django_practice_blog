3b7d2d940f747d39b422fe0e3227b03c069ddb87
Buz Oleksandr <oleksandr.buz@gmail.com>
2017-03-02 15:45:47 +0200

    create urls.py file for resume app, registrate resume model in admin page, added new view for resume app, added new url

--- resume/views.py
+++ resume/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from .models import Resume
 # Create your views here.
+
+
+def resume(request):
+    resume = Resume.objects.all()
+    return render(resume, 'resume/resume.html', {'resume': resume})
